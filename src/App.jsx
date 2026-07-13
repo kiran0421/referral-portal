3bc896f23b4ea1e881fba1a5f7b0d0abfe0045e8
Akshitha <akshitha@example.com>
2026-07-13 10:45:22 +0530

    Added Assignment 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
-import Joinclub from "./components/Joinclub";
-
+import  Header  from "./components/layout/Header";
+import Dashboard from "./pages/Dashboard";
+import ShareModal from "./components/modals/ShareModal";
+import MoreInfoModal from "./components/modals/MoreInfoModal";
+import { ModalProvider, useModal } from "./context/ModalContext";
+function GlobalModals() {
+  const { infoJob, closeMoreInfo, shareJob, closeShare } = useModal();
+  return (
+    <>
+      {infoJob && <MoreInfoModal job={infoJob} onClose={closeMoreInfo} />}
+      {shareJob && <ShareModal job={shareJob} onClose={closeShare} />}
+    </>
+  );
+}
 function App(){
+    const handleRecommend=()=>{
+        console.log('Recommend a friend clicked');
+        
+    }
   return(
-    <Joinclub/>
+    <ModalProvider>
+    <div className="app-shell">
+        <Header/>
+        <main>
+          <Dashboard/>
+        </main>
+        <GlobalModals/>
+    </div>
+</ModalProvider>
   )
 }
 export default App;
